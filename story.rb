e576f3a712779d1ec076bebad45ba0d5adc145b1
Phil Park <Brainiac@Phils-MBP.local>
2016-03-13 14:05:06 -0400

    Completed solutions to story_likes exercise

--- story.rb
+++ story.rb
@@ -2,14 +2,25 @@ require 'pry'
 
 #write a method that accepts two parameters: title and rating
   #the method should print the story title and the rating out of ten
-
+  
+def display_story(x, y)
+	puts "The story #{x} is rated #{y} out of ten"
+end
 
 #show the user a message asking them for a title
 
+puts "What is the Story Title?"
+
 #store the input in a variable
+
+user_title = gets.strip
 
 #show the user a message asking for their rating of the movie out of 100
 
+puts "Rate the story out of ten"
+
 # store the input in a variable
+
+user_rating = gets.strip
 
 display_story(user_title, user_rating)
